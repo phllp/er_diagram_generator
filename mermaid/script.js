4c216e9fe3fa43521441a7827fa616a9855b81f3
phllp <beigerfelipe@gmail.com>
2024-06-18 10:35:24 -0300

    feat: implementation to re-render the diagram when requested

--- mermaid/script.js
+++ mermaid/script.js
@@ -2,13 +2,14 @@ import { generateDiagram } from "./diagram.js";
 import mermaid from "https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs";
 mermaid.initialize({ startOnLoad: true });
 
-const elementoDiagrama = document.getElementById("diagrama");
-const container = document.getElementById("mermaidContainer");
-
 /**
  * Gera o diagrama e renderiza o componente novamente para atualizar a tela
  */
 const updateErDiagram = async () => {
+  removeDiagramComponent();
+  createDiagramComponent();
+  const elementoDiagrama = document.getElementById("diagrama");
+  const container = document.getElementById("mermaidContainer");
   const diagram = await generateDiagram();
   elementoDiagrama.textContent = diagram;
 
@@ -17,9 +18,42 @@ const updateErDiagram = async () => {
   mermaid.init(undefined, container);
 };
 
+const createDiagramComponent = () => {
+  const createIn = document.getElementById("diagramArea");
+  const mermaidContainer = document.createElement("div");
+  mermaidContainer.id = "mermaidContainer";
+
+  const mermaidComponent = document.createElement("pre");
+  mermaidComponent.classList.add("mermaid");
+  mermaidComponent.classList.add("diagram");
+  mermaidComponent.id = "diagrama";
+  mermaidComponent.innerHTML = `erDiagram
+  PLACEHOLDER {
+      string id PK
+      string name
+  }`;
+
+  mermaidContainer.appendChild(mermaidComponent);
+
+  createIn.appendChild(mermaidContainer);
+  console.log("mermaidContainer", mermaidContainer);
+};
+
+const removeDiagramComponent = () => {
+  const removeFrom = document.getElementById("diagramArea");
+  const toRemove = document.getElementById("mermaidContainer");
+  console.log("toRemove", toRemove);
+  if (toRemove) {
+    removeFrom.removeChild(toRemove);
+  }
+};
+
 // Adiciona um evento de clique ao botão para atualizar o diagrama
 document.addEventListener("DOMContentLoaded", () => {
   document
     .getElementById("generateWIthDbBtn")
     .addEventListener("click", updateErDiagram);
+  document
+    .getElementById("limparBtn")
+    .addEventListener("click", removeDiagramComponent);
 });
